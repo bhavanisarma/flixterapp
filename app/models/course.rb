1cde1c6f9f9eef11f5a8b59fa3c79b6a49f35c32
bhavanisarma <bhavani.sarma@gmail.com>
2019-09-14 17:06:22 +0000

    sections

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,6 @@
 class Course < ApplicationRecord
 	belongs_to :member
+	has_many :sections
 	validates :title, presence: true
 	validates :description, presence: true
 	validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
