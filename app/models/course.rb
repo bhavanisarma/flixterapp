a942398b8cdb2a2c3a6e10d273335194f9b2f45c
bhavanisarma <bhavani.sarma@gmail.com>
2019-09-14 15:05:04 +0000

    ok

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ApplicationRecord
-	belongs_to :Member
+	belongs_to :member
 	validates :title, presence: true
 	validates :description, presence: true
 	validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
