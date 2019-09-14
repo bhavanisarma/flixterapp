db07eadaa10b27820fb7feb29743d1c199c4b1c7
bhavanisarma <bhavani.sarma@gmail.com>
2019-09-14 21:59:44 +0000

    image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_14_170940) do
+ActiveRecord::Schema.define(version: 2019_09_14_203107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_09_14_170940) do
     t.integer "member_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["member_id"], name: "index_courses_on_member_id"
   end
 
