244e9653a7f1d5624f390231e636877583eba18d
bhavanisarma <bhavani.sarma@gmail.com>
2019-09-27 03:16:22 +0000

    sections reorder

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ resources :courses, only: [:index, :show]
 resources :lessons, only: [:show]
 namespace :instructor do
 	resources :lessons, only: [:update]
+	resources :sections, only: [:update]
 	resources :sections, only: [] do
 		 resources :lessons, only: [:new, :create]
 		end
